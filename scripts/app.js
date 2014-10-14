1393f29acc26b08a34f45e401321914647ceb476
Nick Brown <nick@nickbrown.me>
2014-10-14 11:44:15 -0700

    basic structure added

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    var app = {
+        initialize: function() {
+            alert('initializing the application!');
+        }
+    };
+
+    window.APP = app;
+
+}());
